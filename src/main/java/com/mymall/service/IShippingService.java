319e24e71fd9dc8bb246fbedc797948cc028f248
wrbh <hxy610939654@live.com>
2020-01-10 16:35:37 +0800

    10/01/2020 shipping commit

---
+++ src/main/java/com/mymall/service/IShippingService.java
@@ -0,0 +1,18 @@
+package com.mymall.service;
+
+import com.github.pagehelper.PageInfo;
+import com.mymall.common.ServerResponse;
+import com.mymall.pojo.Shipping;
+
+public interface IShippingService {
+
+    ServerResponse add(Integer userId, Shipping shipping);
+
+    ServerResponse<String> del(Integer userId,Integer shippingId);
+
+    ServerResponse update(Integer userId, Shipping shipping);
+
+    ServerResponse<Shipping> select(Integer userId, Integer shippingId);
+
+    ServerResponse<PageInfo> list(Integer userId, int pageNum, int pageSize);
+}
